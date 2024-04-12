1698220307c8494e9919abfe866fa1998f04373f
PC <surajtiwari1155@gmail.com>
2024-04-12 22:58:29 +0530

    Added Component for the cards

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+function BookList(){
+  return (
+    <section>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+    </section>
+  )
+}
+
+const Book=()=>{
+  return (
+    <article>
+      <Image/>
+      <Title/>
+      <Author/>
+    </article>
+  )
+}
+
+const Image=()=> <h2>Image Placeholder</h2>
+const Title=()=> <h2>Book title</h2>
+const Author=()=>{
+  return (
+    <h2>Author Name</h2>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  
-);
+root.render(<BookList/>);
 
 
