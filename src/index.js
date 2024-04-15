8fb7f3ea397b5f67d6f3b5ffe145f00682dc5f9c
PC <surajtiwari1155@gmail.com>
2024-04-15 21:26:03 +0530

    Before adding JS in JSX, Will about to add javascript{} in the JSX

--- src/index.js
+++ src/index.js
@@ -18,20 +18,29 @@ function BookList() {
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      {/* <Image /> */}
+      <img src="./Images/Ramayan_BookCover1.jpg" alt="Ramayan by Valmiki Ji" />
+      {/* <Title /> */}
+      <h2>Ramayana Book by Valmiki</h2>
+      {/* <Author /> */}
+      {/* <h2 style={inlinestyle}>Author: Shree Valmiki</h2>; */}
+
+      <h2>Shree Valmiki</h2>
+
     </article>
   );
 };
 
-const Image = () => (
-  <img
-    src="./Images/Ramayan_BookCover1.jpg"
-    alt="Ramayan by Valmiki Ji"
-  />
-);
-const Title = () => <h2>Ramayana Book by Valmiki</h2>;
+// const Image = () => (
+  // Changing this multiples component into a single componenet. directly shifting this img tag into the book compo.
+//   <img
+//     src="./Images/Ramayan_BookCover1.jpg"
+//     alt="Ramayan by Valmiki Ji"
+//   />
+// );
+
+
+// const Title = () => <h2>Ramayana Book by Valmiki</h2>;
 //jsx css here we have added the inline css, 
 //Sometimes it won't work while changing the same property
 // by using an external stylesheet.
@@ -42,15 +51,17 @@ const Title = () => <h2>Ramayana Book by Valmiki</h2>;
 
 // Let's create a object for the css , and use in the return statement of our Author component
 
-const inlinestyle={
-  color:'#617d98',
-  fontSize:'.85rem',
-  marginTop:'0.5rem',
-}
+// shifting this inline style to index.css file
+// const inlinestyle={
+//   color:'#617d98',
+//   fontSize:'.85rem',
+//   marginTop:'0.5rem',
+// }
 
-const Author = () => {
-  return <h2 style={inlinestyle}>Author: Shree Valmiki</h2>;
-};
+
+// const Author = () => {
+//   return <h2 style={inlinestyle}>Author: Shree Valmiki</h2>;
+// };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BookList />);
