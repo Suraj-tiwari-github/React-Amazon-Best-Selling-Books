2467a31d2ec84984d06c1c31475331fd7c66717c
PC <surajtiwari1155@gmail.com>
2024-04-17 17:21:27 +0530

    Remove key props warning and made the list dynamic

--- src/index.js
+++ src/index.js
@@ -6,69 +6,58 @@ const author = "Shree Valmiki";
 const title = "Ramayana Book by Valmiki";
 const img = "./Images/Ramayan_BookCover1.jpg";
 
-const firstBook={
-   author :"Shree Valmiki",
-   title : "Ramayana Book by Valmiki",
-   img :"./Images/Ramayan_BookCover1.jpg",
-}
+const books = [
+  {
+    author: "Shree Valmiki",
+    title: "Ramayana Book by Valmiki",
+    img: "./Images/Ramayan_BookCover1.jpg",
+    id:1,
+  },
+  {
+    author: "Ved vyas",
+    title: "Mahabharat",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1akxg7HB1ykbtj_hjZmim-NDed-dC1j4exVxTrd2FVXTLyWqn",
+    id:2,
+  },
+];
 
-const secondBook = {
-  author: "Ved vyas",
-  title: "Mahabharat",
-  img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1akxg7HB1ykbtj_hjZmim-NDed-dC1j4exVxTrd2FVXTLyWqn",
-};
+// const firstBook={
+//    author :"Shree Valmiki",
+//    title : "Ramayana Book by Valmiki",
+//    img :"./Images/Ramayan_BookCover1.jpg",
+// }
+
+// const secondBook = {
+//   author: "Ved vyas",
+//   title: "Mahabharat",
+//   img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1akxg7HB1ykbtj_hjZmim-NDed-dC1j4exVxTrd2FVXTLyWqn",
+// };
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      >
-        {/* here we have added a children props
-        the component has to be rendered in <book> here goes the children props </bo0k> */}
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam
-          velit nemo consequuntur debitis rerum ad blanditiis necessitatibus.
-          Rem distinctio minima sit architecto fugit quis animi sunt. Atque
-          dolore dignissimos esse?
-        </p>
-        <button>Submit</button>
-      </Book>
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book)=>{
+        console.log(book)
+        const {img,title, author, id}=book;
+        return (
+          // here we have added the key to avoid the unique key props warming in the console. 
+          <Book img={img} title={title} author={author} key={id}/>
+        )
+      })}
     </section>
   );
 }
 
 // here we are going to use function destructuring.
-const Book = ({img, title, author, children}) => {
-  // console.log(props)
-  // const {img, title, author}=props;
+const Book = ({ img, title, author, children }) => {
+  
   return (
     <article className="book">
-      {/* <Image /> */}
-      {/* <Title /> */}
-      {/* <Author /> */}
-      {/* <h2 style={inlinestyle}>Author: Shree Valmiki</h2>; */}
-      {/* <img src="./Images/Ramayan_BookCover1.jpg" alt="Ramayan by Valmiki Ji" />
-      <h2>Ramayana Book by Valmiki</h2>
-      <h2>Shree Valmiki</h2> */}
-
+      
       <img src={img} alt="Ramayan by Valmiki Ji" />
       <h2>{title}</h2>
       <h2>{author}</h2>
-      {/* here we are rendering the children props */}
-      {children}
-      {/* // we cannot use statement like let x=6 in the js under component. for example */}
-      {/* {<p> {let x=6 }</p>} */}
-      {/* the above line will through an error because it is a statement, there have to return something */}
-      {/* <p>{6+6}</p> */}
-
+      
     </article>
   );
 };
